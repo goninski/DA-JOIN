18073b81e508a6dfdefc3a08deca09fd064e8bf5
goninski <webadmin@goninski.ch>
2025-03-30 06:01:39 +0200

    add main templates via js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function init() {
+    getMainTemplates()
 }
 
+function getMainTemplates() {
+    getHeader();
+    getSidemenu();
+}
+
+function getHeader() {
+    document.getElementById('header').innerHTML = getHeaderTemplate();
+}
+
+function getSidemenu() {
+    let sideMenuRef = document.getElementById('sideMenu');
+    sideMenuRef.innerHTML = getSideMenuTemplate();
+    sideMenuRef.classList.add('hide--ss-mob');
+
+    let sideMenuMobRef = document.getElementById('sideMenuMob');
+    sideMenuMobRef.innerHTML = getSideMenuMobTemplate();
+    sideMenuMobRef.classList.add('show--ss-mob');
+}
---
+++ scripts/templates.js
@@ -0,0 +1,23 @@
+function getHeaderTemplate() {
+    return `
+        <div class="ct-container flex-row gap justify-between flex-wrap--ss-mob py">
+            <div class="">Kanban Projekt Management Tool</div>
+            <div class="flex-row gap">
+                <img src="./assets/icons/icon-ipsum-black.svg" alt="">
+                <img src="./assets/icons/icon-ipsum-black.svg" alt="">
+            </div>
+        </div>
+    `
+}
+
+function getSideMenuTemplate() {
+    return `
+        <a href="./">Home</a>
+    `
+}
+
+function getSideMenuMobTemplate() {
+    return `
+        <a href="./">Home</a>
+    `
+}
