c4f65caffe500365fb4b550d217ff5fdaf3d09a4
goninski <webadmin@goninski.ch>
2025-03-31 12:54:18 +0200

    asset library additions

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,7 +1,7 @@
 function getHeaderTemplate() {
     return `
         <div class="ct-container flex-row gap justify-between flex-wrap--ss-mob py">
-            <div class="">Kanban Projekt Management Tool</div>
+            <div><a href="./">Kanban Projekt Management Tool</a></div>
             <div class="flex-row gap">
                 <img src="./assets/icons/icon-ipsum-black.svg" alt="">
                 <img src="./assets/icons/icon-ipsum-black.svg" alt="">
