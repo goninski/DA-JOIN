99b359f4d61bbaa159ebe82eb671faa04fb3c52b
goninski <webadmin@goninski.ch>
2025-04-03 16:32:41 +0200

    testdata in data.js

--- scripts/data.js
+++ scripts/data.js
@@ -1,3 +1,73 @@
+// *** TEST DATA
+let tasks = [];
+let contacts = [];
+let categories = [];
+
+tasks = [
+    {
+        "id": 0,
+        "title": "Title ...",
+        "description": "Description...",
+        "dueDate": 0,
+        "priority": "low",
+        "assignedTo": [0, 1, 2],
+        "category": "Technical Task",
+        "subtasks": []
+    }
+];
+
+categories = [
+    {
+        "id": 0,
+        "name": "Technical Task"
+    },
+    {
+        "id": 0,
+        "name": "User Story"
+    }
+];
+
+contacts = [
+    {
+        "id": 0,
+        "name": "François Gonin",
+        "email": "mail@goninski.dev",
+        "phone": "+41 78 888 77 66",
+        "initials": "FG",
+        "color": "violet"
+    },
+    {
+        "id": 1,
+        "name": "Julian Kraske",
+        "email": "",
+        "phone": "",
+        "initials": "JK",
+        "color": "pink"
+    },
+    {
+        "id": 2,
+        "name": "Nico Hässler",
+        "email": "",
+        "phone": "",
+        "initials": "NH",
+        "color": "orange"
+    },
+    {
+        "id": 3,
+        "name": "Vivienne Wündisch",
+        "email": "",
+        "phone": "",
+        "initials": "VW",
+        "color": "lightblue"
+    }
+
+];
+
+
+
+
+
+
 function init() {
     getMainTemplates();
 }
