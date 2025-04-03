cc3516bb89847e2ea59a21803aa6df5fbbeb591e
goninski <webadmin@goninski.ch>
2025-04-03 16:52:33 +0200

    fix data.js

--- scripts/data.js
+++ scripts/data.js
@@ -1,20 +1,7 @@
 // *** TEST DATA
-let tasks = [];
-let contacts = [];
 let categories = [];
-
-tasks = [
-    {
-        "id": 0,
-        "title": "Title ...",
-        "description": "Description...",
-        "dueDate": 0,
-        "priority": "low",
-        "assignedTo": [0, 1, 2],
-        "category": "Technical Task",
-        "subtasks": []
-    }
-];
+let contacts = [];
+let tasks = [];
 
 categories = [
     {
@@ -63,57 +50,18 @@ contacts = [
 
 ];
 
-
-
-
-
-
-function init() {
-    getMainTemplates();
-}
-
-function getMainTemplates() {
-    getHeader();
-    getSidemenu();
-}
-
-function getHeader() {
-    document.getElementById('header').innerHTML = getHeaderTemplate();
-}
-
-function getSidemenu() {
-    let sideMenuRef = document.getElementById('sideMenu');
-    sideMenuRef.innerHTML = getSideMenuTemplate();
-    sideMenuRef.classList.add('hide--ss-mob');
-
-    let sideMenuMobRef = document.getElementById('sideMenuMob');
-    sideMenuMobRef.innerHTML = getSideMenuMobTemplate();
-    sideMenuMobRef.classList.add('show--ss-mob');
-}
-
-function resetInput(event, id) {
-    document.getElementById(id).reset()
-    event.preventDefault();
-}
-
-
-
-
-// TEMPLATE CALLS FOR TASK INPUTS
-function getContactSelectOptions(id = 'inputContacts') {
-    let selectInput = document.getElementById(id);
-    selectInput.innerHTML = '';
-    for (let index = 0; index < contacts.length; index++) {
-        selectInput.innerHTML += getContactSelectOptionTemplate(contacts[index]);
+tasks = [
+    {
+        "id": 0,
+        "title": "Title ...",
+        "description": "Description...",
+        "dueDate": 0,
+        "priority": "low",
+        "assignedTo": [0, 1, 2],
+        "category": "Technical Task",
+        "subtasks": []
     }
-}
+];
 
-function getCategorySelectOptions(id = 'inputCategory') {
-    let selectInput = document.getElementById(id);
-    selectInput.innerHTML = '';
-    for (let index = 0; index < categories.length; index++) {
-        selectInput.innerHTML += getCategorySelectOptionTemplate(categories[index]);
-    }
-}
 
 
