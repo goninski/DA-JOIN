8d732b515ed537e547553641869fff5a4a7bec54
Vivienne Wündisch <vivi.wuendisch@me.com>
2025-04-27 00:44:25 +0200

    Changes Login Style and Check-Login-Funktion

--- login.js
+++ login.js
@@ -1,15 +1,43 @@
 window.onload = () => {
     const login_logo = document.getElementById("login_logo");
     const login_content = document.getElementById("login-main-content");
-  
-    // Nach 2 Sekunden: Logo verschieben
+
     setTimeout(() => {
         login_logo.classList.add("moved");
     }, 100);
-  
-    // Nach 3 Sekunden: Content einblenden
+
     setTimeout(() => {
         login_content.classList.add("visible");
     }, 100);
-  };
-  
+};
+
+// usersDatabase soll hier die Datenbank mit den User Zugangsdaten symbolisieren - muss noch geändert werden
+// function checkLogin(event) {
+//     event.preventDefault();
+//     console.log('Login function called');
+
+//     const emailField = document.getElementById('email');
+//     const passwordField = document.getElementById('pwd');
+
+//     const emailInput = emailField.value.trim();
+//     const passwordInput = passwordField.value.trim();
+
+//     let userFound = null;
+
+//     //for (let i = 0; i < usersDatabase.length; i++) {
+//         //const user = usersDatabase[i];
+//         if (user.email === emailInput && user.password === passwordInput) {
+//             userFound = user;
+//         }
+    
+
+//     if (userFound) {
+//         alert('Login erfolgreich!');
+//         window.location.href = 'summary.html';
+//     } else {
+//         alert('E-Mail oder Passwort ist falsch.');
+//         emailField.classList.add('false_input');
+//         passwordField.classList.add('false_input');
+//     }
+// }
+
